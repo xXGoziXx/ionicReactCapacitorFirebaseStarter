80b0a15e26ed513602cfe7ef73e4552f22a45775
Godstime Osarobo <nosarobo@gmail.com>
2024-12-11 04:54:06 +0000

    clean up

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8kjvktmo558"
+    "revision": "0.6p59k8gveh"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
